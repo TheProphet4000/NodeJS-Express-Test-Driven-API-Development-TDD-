b302f680053293afb0ed7cbf943075f13653a6bf
Mikkel Andreasen <38246850+TheProphet4000@users.noreply.github.com>
2021-11-22 14:29:34 +0100

    Prøvede at rette fejl.. hjalp ikke

--- app.test.js
+++ app.test.js
@@ -24,37 +24,35 @@ describe("Todos API", () => {
 
 it("GET /todos/id --> specific todo by ID", () => {
     return request(app)
-        .get("/todos/1")
-        .expect("Content-Type", /json/)
-        .expect(200)
-        .then(response => {
-            expect(response.body).toEqual(
-                expect.objectContaining({
-                    name: expect.any(String),
-                    completed: expect.any(Boolean),
-                }),
-            );
-        });
+    .get("/todos/1")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then(response => {
+        expect(response.body).toEqual(expect.objectContaining({
+        name: expect.any(String),
+        completed: expect.any(Boolean) }));
+    }   );
 });
 
 it("GET /todos/id --> 404 didn't find it", () => {
-    return request(app).app("/todos/999999999").expect(404);
+    return request(app)
+    .app("/todos/2142")
+    .expect(404);
 });
 
 it("POST /todos --> created todo", () => {
-    return request(app).post("/todos").send({
-        name: "wakeup"
-    }).expect("Content-Type", /json/).expect(201)
-        .then(response => {
-            expect(response.body).toEqual(
-                expect.objectContaining({
-                    name: "wakeup",
-                    completed: false,
-                })
-            );
-        });
+    return request(app)
+    .post("/todos")
+    .send({ name: "wakeup"})
+    .expect("Content-Type", /json/).expect(201)
+    .then(response => { expect(response.body).toEqual(
+        expect.objectContaining({ name: "wakeup", completed: false}));
+    });
 });
 
-it("GET /todos --> validates request body", () => {
-    return request(app).post("/todos").send({ name: 123}).expect(422);
+it("POST /todos --> validates request body", () => {
+    return request(app)
+    .post("/todos")
+    .send({ name: 123})
+    .expect(422);
 });
